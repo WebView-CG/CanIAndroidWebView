dc4cf0883a1183442add22aa69a22eda5a8baf04
Niklas Merz <niklasmerz@apache.org>
2025-09-28 17:27:54 +0200

    Version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.caniwebview.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 100
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
